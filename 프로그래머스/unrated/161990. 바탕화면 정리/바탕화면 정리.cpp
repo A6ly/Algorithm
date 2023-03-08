525e4443ad189c9f7dbe635ae047630caad7ed3c
Gihyeon Kim <a6ly26@gmail.com>
2023-03-09 00:13:20 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.cpp
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -6,7 +6,6 @@ using namespace std;
 vector<int> solution(vector<string> wallpaper) 
 {
     vector<int> answer;
-
     int lux = 50, luy = 50, rdx = -1, rdy = -1;
 
     for (int i = 0; i < wallpaper.size(); i++)
