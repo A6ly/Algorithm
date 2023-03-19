325cdc42390bb2840dbef8240a992dc0a0f08bb8
Gihyeon Kim <a6ly26@gmail.com>
2023-03-19 21:37:23 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.cpp
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.cpp
@@ -3,157 +3,87 @@
 
 using namespace std;
 
+string answer = "";
+pair<int, int> rightFinger = { 3, 0 }, leftFinger = { 3, 2 };
+
+void middleNumber(int y, int x, string hand)
+{
+	int rightDistance = abs(rightFinger.first - y) + abs(rightFinger.second - x);
+	int leftDistance = abs(leftFinger.first - y) + abs(leftFinger.second - x);
+
+	if (rightDistance < leftDistance)
+	{
+		rightFinger = { y, x };
+		answer += 'R';
+	}
+	else if (rightDistance > leftDistance)
+	{
+		leftFinger = { y, x };
+		answer += 'L';
+	}
+	else
+	{
+		if (hand == "right")
+		{
+			rightFinger = { y, x };
+			answer += 'R';
+		}
+		else if (hand == "left")
+		{
+			leftFinger = { y, x };
+			answer += 'L';
+		}
+	}
+}
+
 string solution(vector<int> numbers, string hand)
 {
-	string answer = "";
-	pair<int, int> right = { 3, 0 }, left = { 3, 2 };
-
 	for (int i = 0; i < numbers.size(); i++)
 	{
 		if (numbers[i] == 0)
 		{
-			int rightDistance = abs(right.first - 3) + abs(right.second - 1);
-			int leftDistance = abs(left.first - 3) + abs(left.second - 1);
-
-			if (rightDistance < leftDistance)
-			{
-				right = { 3, 1 };
-				answer += 'R';
-			}
-			else if (rightDistance > leftDistance)
-			{
-				left = { 3, 1 };
-				answer += 'L';
-			}
-			else
-			{
-				if (hand == "right")
-				{
-					right = { 3, 1 };
-					answer += 'R';
-				}
-				else if (hand == "left")
-				{
-					left = { 3, 1 };
-					answer += 'L';
-				}
-			}
+			middleNumber(3, 1, hand);
 		}
 		else if (numbers[i] == 1)
 		{
-			left = { 0, 0 };
+			leftFinger = { 0, 0 };
 			answer += 'L';
 		}
 		else if (numbers[i] == 2)
 		{
-			int rightDistance = right.first + abs(right.second - 1);
-			int leftDistance = left.first + abs(left.second - 1);
-
-			if (rightDistance < leftDistance)
-			{
-				right = { 0, 1 };
-				answer += 'R';
-			}
-			else if (rightDistance > leftDistance)
-			{
-				left = { 0, 1 };
-				answer += 'L';
-			}
-			else
-			{
-				if (hand == "right")
-				{
-					right = { 0, 1 };
-					answer += 'R';
-				}
-				else if (hand == "left")
-				{
-					left = { 0, 1 };
-					answer += 'L';
-				}
-			}
+			middleNumber(0, 1, hand);
 		}
 		else if (numbers[i] == 3)
 		{
-			right = { 0, 2 };
+			rightFinger = { 0, 2 };
 			answer += 'R';
 		}
 		else if (numbers[i] == 4)
 		{
-			left = { 1, 0 };
+			leftFinger = { 1, 0 };
 			answer += 'L';
 		}
 		else if (numbers[i] == 5)
 		{
-			int rightDistance = abs(right.first - 1) + abs(right.second - 1);
-			int leftDistance = abs(left.first - 1) + abs(left.second - 1);
-
-			if (rightDistance < leftDistance)
-			{
-				right = { 1, 1 };
-				answer += 'R';
-			}
-			else if (rightDistance > leftDistance)
-			{
-				left = { 1, 1 };
-				answer += 'L';
-			}
-			else
-			{
-				if (hand == "right")
-				{
-					right = { 1, 1 };
-					answer += 'R';
-				}
-				else if (hand == "left")
-				{
-					left = { 1, 1 };
-					answer += 'L';
-				}
-			}
+			middleNumber(1, 1, hand);
 		}
 		else if (numbers[i] == 6)
 		{
-			right = { 1, 2 };
+			rightFinger = { 1, 2 };
 			answer += 'R';
 		}
 		else if (numbers[i] == 7)
 		{
-			left = { 2, 0 };
+			leftFinger = { 2, 0 };
 			answer += 'L';
 		}
 		else if (numbers[i] == 8)
 		{
-			int rightDistance = abs(right.first - 2) + abs(right.second - 1);
-			int leftDistance = abs(left.first - 2) + abs(left.second - 1);
-
-			if (rightDistance < leftDistance)
-			{
-				right = { 2, 1 };
-				answer += 'R';
-			}
-			else if (rightDistance > leftDistance)
-			{
-				left = { 2, 1 };
-				answer += 'L';
-			}
-			else
-			{
-				if (hand == "right")
-				{
-					right = { 2, 1 };
-					answer += 'R';
-				}
-				else if (hand == "left")
-				{
-					left = { 2, 1 };
-					answer += 'L';
-				}
-			}
+			middleNumber(2, 1, hand);
 		}
 		else if (numbers[i] == 9)
 		{
-			right = { 2, 2 };
+			rightFinger = { 2, 2 };
 			answer += 'R';
 		}
 	}
