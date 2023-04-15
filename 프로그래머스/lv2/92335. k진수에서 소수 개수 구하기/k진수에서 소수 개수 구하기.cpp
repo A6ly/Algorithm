830b0059286b4bdef6635951060f4053147b84f8
Gihyeon Kim <a6ly26@gmail.com>
2023-04-15 18:58:14 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
@@ -0,0 +1,74 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+bool IsPrime(long long num)
+{
+	if (num <= 1)
+	{
+		return false;
+	}
+
+	for (long long i = 2; i <= sqrt(num); i++)
+	{
+		if (num % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+int solution(int n, int k)
+{
+	int answer = 0;
+	string str = "";
+
+	while (n > 0)
+	{
+		str = to_string(n % k) + str;
+		n /= k;
+	}
+
+	int pos = 0;
+
+	while (!str.empty())
+	{
+		if ((pos = str.find('0')) != string::npos)
+		{
+			if (pos == 0)
+			{
+				str.erase(0, 1);
+			}
+			else
+			{
+				string temp = str.substr(0, pos);
+
+				if (!temp.empty())
+				{
+					if (IsPrime(stoll(temp)))
+					{
+						answer++;
+					}
+				}
+
+				str.erase(0, pos + 1);
+			}
+		}
+		else
+		{
+			if (IsPrime(stoll(str)))
+			{
+				answer++;
+			}
+
+			break;
+		}
+	}
+
+	return answer;
+}
