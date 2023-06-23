8368cc4b65c6018eb92a7649acdf1c8e5c923e62
Gihyeon Kim <a6ly26@gmail.com>
2023-06-24 02:49:30 +0900

    [level 2] Title: 주식가격, Time: 14.22 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices)
+{
+	vector<int> answer(prices.size());
+	stack<int> st;
+
+	for (int i = 0; i < prices.size(); i++)
+	{
+		while (!st.empty() && prices[i] < prices[st.top()])
+		{
+			answer[st.top()] = i - st.top();
+			st.pop();
+		}
+
+		st.push(i);
+	}
+
+	while (!st.empty())
+	{
+		answer[st.top()] = prices.size() - 1 - st.top();
+		st.pop();
+	}
+
+	return answer;
+}
