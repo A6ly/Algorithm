d9f388204ecc13504713b2da216d544c3703af28
Gihyeon Kim <a6ly26@gmail.com>
2023-06-24 03:06:34 +0900

    [level 2] Title: 더 맵게, Time: 226.04 ms, Memory: 40.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K)
+{
+	int answer = 0;
+	priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
+
+	while (pq.size() >= 2 && pq.top() < K)
+	{
+		int first = pq.top();
+		pq.pop();
+		int second = pq.top();
+		pq.pop();
+
+		pq.push(first + second * 2);
+		answer++;
+	}
+
+	if (pq.top() < K)
+	{
+		answer = -1;
+	}
+
+	return answer;
+}
